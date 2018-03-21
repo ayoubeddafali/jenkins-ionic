5d86359831859f3e16b035bbf2df32d23d7f41c1
ayoubensalem <ayoubensalem@gmail.com>
2018-03-21 00:02:50 +0000

    update jenkins service

--- src/providers/jenkins.ts
+++ src/providers/jenkins.ts
@@ -6,13 +6,18 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class Jenkins {
-    Token = `2ffd011a1070fe12c29a722fb70a48c4`;
-    ID = `ayoub`;
-    baseUrl = `http://ayoub:2ffd011a1070fe12c29a722fb70a48c4@172.16.88.143:8080/api/json`;
+    token = `2ffd011a1070fe12c29a722fb70a48c4`;
+    username = `ayoub`;
+    password = "19641995"; 
+    baseUrl = `/api`;
+    
     constructor(public http: Http){ console.log("JenkinsServiceConstructore") ; }
 
     getAllJobs(){
-        return this.http.get(this.baseUrl)
+        let headers = new Headers();
+        headers.append("Authorization", "Basic " + btoa(this.username + ":" + this.password)); 
+        headers.append("Content-Type", "application/x-www-form-urlencoded");
+        return this.http.get(this.baseUrl, {headers: headers})
             .map(res => res.json())
 
     }
